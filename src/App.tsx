16f276c311dbcb013f212836aa19228006458d5f
munkhuu1126 <munkh1151@gmail.com>
2022-05-10 15:35:15 +0800

    added question counter, react-slick(needs fix)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,95 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 import { Button } from '@chakra-ui/react'
 import Question from './components/Question';
+import Slider from 'react-slick'
+
+
+interface Questions {
+  id: number,
+  question: string,
+  correctAnswer: string,
+  answers: string[]
+}
+
+
+
 
 function App() {
+  const questions: Questions[] = [
+    {
+      id: 1,
+      question: "Хамгийн анхны блокчэйн гэрлэлт Америкийн аль мужид болсон бэ? ",
+      correctAnswer: "Калифорн",
+      answers: ['Калифорн', 'Нью Юорк', 'Вашингтон', 'Улаанбаатар']
+
+    },
+    {
+      id: 2,
+      question: "Хамгийн анхны блокчэйн гэрлэлт Америкийн аль мужид болсон бэ? ",
+      correctAnswer: "Калифорн",
+      answers: ['Калифорн', 'Нью Юорк', 'Вашингтон', 'Улаанбаатар']
+
+    },
+    {
+      id: 3,
+      question: "Хамгийн анхны блокчэйн гэрлэлт Америкийн аль мужид болсон бэ? ",
+      correctAnswer: "Калифорн",
+      answers: ['Калифорн', 'Нью Юорк', 'Вашингтон', 'Улаанбаатар']
+
+    },
+    {
+      id: 4,
+      question: "Хамгийн анхны блокчэйн гэрлэлт Америкийн аль мужид болсон бэ? ",
+      correctAnswer: "Калифорн",
+      answers: ['Калифорн', 'Нью Юорк', 'Вашингтон', 'Улаанбаатар']
+
+    },
+    {
+      id: 5,
+      question: "Хамгийн анхны блокчэйн гэрлэлт Америкийн аль мужид болсон бэ? ",
+      correctAnswer: "Калифорн",
+      answers: ['Калифорн', 'Нью Юорк', 'Вашингтон', 'Улаанбаатар']
+
+    },
+    {
+      id: 6,
+      question: "Хамгийн анхны блокчэйн гэрлэлт Америкийн аль мужид болсон бэ? ",
+      correctAnswer: "Калифорн",
+      answers: ['Калифорн', 'Нью Юорк', 'Вашингтон', 'Улаанбаатар']
+
+    },
+    {
+      id: 7,
+      question: "Хамгийн анхны блокчэйн гэрлэлт Америкийн аль мужид болсон бэ? ",
+      correctAnswer: "Калифорн",
+      answers: ['Калифорн', 'Нью Юорк', 'Вашингтон', 'Улаанбаатар']
+
+    },
+
+
+  ]
+
+
+  const [start, setStart] = useState(false)
+  const [totalScore, setTotalScore] = useState(0)
+
   return (
-    <div className="App">
-      <div className="flex justify-center align-middle items-center min-h-[100vh]">
-        <Question />
+    <div className="App align-middle">
+      <div className="flex justify-center items-center min-h-[100vh]">
+        {
+          start ? (
+            <div className="">
+              <Question questions={questions} setStart={setStart} setTotalScore={setTotalScore} totalScore={totalScore} />
+            </div>) :
+            <Button
+              size={'lg'}
+              onClick={() => setStart(true)}
+            >
+              Start
+            </Button>
+        }
+
       </div>
     </div>
   );
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,27 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import {
-    Button, ButtonGroup, Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton, Stack, useDisclosure
+    Button, Stack
 } from '@chakra-ui/react'
+import Slider from 'react-slick'
 
-export default function Question() {
-    const questions = [
-        {
-            "№": 1,
-            "question": "Хамгийн анхны блокчэйн гэрлэлт Америкийн аль мужид болсон бэ? ",
-            "correctAnswer": "Калифорн",
-            "answers": ['Калифорн', 'Нью Юорк', 'Вашингтон', 'Улаанбаатар']
+interface Props {
+    setStart: React.Dispatch<React.SetStateAction<any>>
+    setTotalScore: React.Dispatch<React.SetStateAction<any>>
+    totalScore: number,
+    questions: {
+        id: number,
+        question: string,
+        correctAnswer: string,
+        answers: string[]
+    }[]
+}
 
-        },
-    ]
+export default function Question({ questions, setStart, setTotalScore, totalScore }: Props) {
+
 
     const [isCorrect, setIsCorrect] = useState(false)
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [questionCounter, setQuestionCounter] = useState(0)
+    const slickRef = useRef<any>()
+    const settings = {
+        className: 'center',
+        dots: false,
+        infinite: false,
+        speed: 500,
+        delay: 5500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+        arrows: false,
+
+    };
+
+    //Score updates here
+    useEffect(() => {
+        if (isCorrect === true) {
+
+            console.log(totalScore)
+        }
+    }, [totalScore])
+
+    const handleChoice = (way: string) => {
+        if (slickRef.current && way === 'next') {
+            slickRef.current.slickNext();
+        }
+    }
 
     const questionComponents = (
         questions.map((question, index) => {
@@ -29,22 +55,28 @@ export default function Question() {
             const checkAnswer = (answer: string): void => {
                 if (answer === question.correctAnswer) {
                     console.log('your answer is correct')
+                    setTotalScore((prevScore: number): number => prevScore + 1)
                     setIsCorrect(true)
+                    setQuestionCounter(counter => counter + 1)
+
+
                 }
                 else {
                     console.log('your answer is incorrect')
                     setIsCorrect(false)
+                    setQuestionCounter(counter => counter + 1)
                 }
             }
 
             return (
                 <div key={index} className="space-y-4">
+                    <p className="font-bold">{questionCounter}/{questions.length}</p>
                     <h1 className="bg-yellow-200 text-xl font-bold p-10 rounded-lg">{question.question}</h1>
                     <Stack className=" flex items-center justify-between p-10" >
                         {question.answers.map((answer, index) => {
                             return (
                                 <div key={index}>
-                                    <Button width={500} onClick={() => { checkAnswer(answer) }}>{answer}</Button>
+                                    <Button width={500} onClick={() => { checkAnswer(answer); handleChoice('next'); }}>{answer}</Button>
                                 </div>
                             )
                         })}
@@ -56,26 +88,11 @@ export default function Question() {
     )
     return (
         <div>
-            {questionComponents}
+            <Slider {...settings} ref={slickRef}>
+                {questionComponents}
+            </Slider>
             <div className="flex justify-center">
-                <Button onClick={onOpen}>Open Modal</Button>
 
-                <Modal isOpen={isOpen} onClose={onClose}>
-                    <ModalOverlay />
-                    <ModalContent>
-                        <ModalHeader>{isCorrect ? 'Your Answer Is Correct' : 'Your Answer Is Incorrect :('}</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                            {/* put something here */}
-                        </ModalBody>
-
-                        <ModalFooter>
-                            <Button colorScheme='blue' mr={3} onClick={onClose}>
-                                Close
-                            </Button>
-                        </ModalFooter>
-                    </ModalContent>
-                </Modal>
             </div>
         </div>
     )
